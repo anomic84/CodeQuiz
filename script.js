79a5cd082a1526c40660e5366d2ebb6566954364
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2022-11-28 16:33:57 -0800

    added the html body and guts, added most of the css. Psuedo coded the js functions

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Start the quiz function
+
+// Start timer
+// change from intro to questions
+
+
+
+// next question function
+
+
+// answer right funtion (maybe inside each questions function)
+// answer wrong funtion (maybe inside each questions function)
+
+// last question function to change to finish
+
+
+
+// enter highscore function
+//Go back function
+//
+
+
+
+//highscore button function at intro
+
+
 
